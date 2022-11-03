37790ce4d1e80c61f988903b71bd6bd5471b05b7
nazmulhassan-tmpl <nazmul.h@tunicalabsmedia.com>
2022-11-03 22:31:18 +0600

    fixed responsive issue in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,26 @@
-import { AppBar, Toolbar, Box, Typography, styled, Button, Tabs, Tab } from '@mui/material'
+import {
+  AppBar,
+  Toolbar,
+  Box,
+  Typography,
+  styled,
+  Button,
+  Tabs,
+  Tab,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material'
 import logo from '../../public/img/logo2.png'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from 'react'
+import DrawerComponent from './Drawer'
 
 const Header = styled(AppBar)`
   background: transparent;
   box-shadow: none;
   color: #678c92;
-  padding: 20px;
+  // padding: 20px;
 `
 
 const Nav = styled(Box)`
@@ -18,27 +31,44 @@ const Nav = styled(Box)`
 `
 
 const Navbar = () => {
+  const [value, setValue] = useState('one')
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
+  }
+
+  const theme = useTheme()
+  // console.log(theme)
+  const isMatch = useMediaQuery(theme.breakpoints.down('md'))
+  const isMatch2 = useMediaQuery(theme.breakpoints.down('sm'))
+
   return (
-    <Header position='static'>
+    <Header className={isMatch2 ? 'p-none' : 'p-20'} position='fixed'>
       <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Link href='/'>
-          <Image src={logo} alt='/logo' width={0} height={50} />
-        </Link>
-        <Nav>
-          <Link href='/about'>
-            <Typography>About</Typography>
-          </Link>
-          <Link href='/services'>
-            <Typography>Services</Typography>
-          </Link>
-          <Link href='/projects'>
-            <Typography>Projects</Typography>
-          </Link>
-          <Link href='/contact'>
-            <Typography>Contact</Typography>
-          </Link>
-        </Nav>
-        <Button variant='contained'>Need a Project?</Button>
+        {isMatch ? (
+          <DrawerComponent />
+        ) : (
+          <>
+            <Link href='/'>
+              <Image src={logo} alt='/logo' width={0} height={50} />
+            </Link>
+            <Nav>
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                textColor='secondary'
+                indicatorColor='secondary'
+                aria-label='secondary tabs example'
+              >
+                <Tab value='one' label='About' />
+                <Tab value='two' label='Services' />
+                <Tab value='three' label='Projects' />
+                <Tab value='three' label='Contact' />
+              </Tabs>
+            </Nav>
+            <Button variant='contained'>Need a Project?</Button>
+          </>
+        )}
       </Toolbar>
     </Header>
   )
