376f963079020cb82d944977d861473905ec4c3f
nazmulhassan-tmpl <nazmul.h@tunicalabsmedia.com>
2022-11-09 16:38:24 +0600

    completed company section

--- src/components/HomePage/Company/index.js
+++ src/components/HomePage/Company/index.js
@@ -1,21 +1,55 @@
-import { CompanyContainer, CompanyImageContainer } from "../../../styles/homepage/company";
+import { Typography, Button } from '@mui/material'
+import {
+  CompanyContainer,
+  CompanyDetails,
+  CompanyImage,
+  CompanyImageContainer,
+  CompanyInfo,
+  CompanyInfoDescription,
+  CompanyInfoTitle,
+} from '../../../styles/homepage/company'
 import Image from 'next/image'
 import company1 from '/public/img/company/company-1.svg'
 import company2 from '/public/img/company/company-2.svg'
 import company3 from '/public/img/company/company-3.svg'
 import company4 from '/public/img/company/company-4.svg'
 import company5 from '/public/img/company/company-5.svg'
+import companyImage from '/public/img/company/companyImage.svg'
 
 export default function Company() {
-    return(
-        <CompanyContainer>
-            <CompanyImageContainer>
-                <Image src={company1} layout='responsive' ></Image>
-                <Image src={company2} layout='responsive' ></Image>
-                <Image src={company3} layout='responsive' ></Image>
-                <Image src={company4} layout='responsive' ></Image>
-                <Image src={company5} layout='responsive' ></Image>
-            </CompanyImageContainer>
-        </CompanyContainer>
-    )
+  return (
+    <CompanyContainer>
+      {/* company logo  */}
+      <CompanyImageContainer>
+        <Image src={company1} layout='responsive'></Image>
+        <Image src={company2} layout='responsive'></Image>
+        <Image src={company3} layout='responsive'></Image>
+        <Image src={company4} layout='responsive'></Image>
+        <Image src={company5} layout='responsive'></Image>
+      </CompanyImageContainer>
+      {/* company details  */}
+      <CompanyDetails>
+        <CompanyImage>
+          <Image src={companyImage} layout='responsive'></Image>
+        </CompanyImage>
+        <CompanyInfo>
+          <Typography variant='subtitle' sx={{ color: '#5BCCDE' }}>
+            About Us
+          </Typography>
+          <CompanyInfoTitle variant='h3'>
+            Owner and investor with a reputation
+          </CompanyInfoTitle>
+          <CompanyInfoDescription>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc elit
+            sem, vestibulum sed leo eu, malesuada pharetra mauris. Integer
+            rhoncus, eros vel euismod tempor, ipsum magna tristique nisi, quis
+            ullamcorper enim magna eu orci. Sed semper ex quis semper aliquet.
+            Cras hendrerit molestie sapien sed fermentum. Mauris dui tortor,
+            viverra vel ultrices in, congue sed ex.
+          </CompanyInfoDescription>
+          <Button variant='contained' sx={{padding: '5px 40px'}}>Explore</Button>
+        </CompanyInfo>
+      </CompanyDetails>
+    </CompanyContainer>
+  )
 }
--- src/styles/homepage/company/index.js
+++ src/styles/homepage/company/index.js
@@ -1,29 +1,58 @@
-import {Box} from '@mui/material'
-import {styled} from '@mui/material/styles'
-import {Colors} from '../../theme'
+import { Box, Typography } from '@mui/material'
+import { styled } from '@mui/material/styles'
+import { Colors } from '../../theme'
 
-export const CompanyContainer = styled(Box)(({theme})=> ({
-    width: '100%',
-    height: 600,
-    marginTop: 150,
-    padding: '30px 0px',
-    background: Colors.secondary,
+export const CompanyContainer = styled(Box)(({ theme }) => ({
+  width: '100%',
+  // height: 600,
+  marginTop: 150,
+  padding: '30px 0px',
+  background: Colors.secondary,
 }))
 
-export const CompanyImageContainer = styled(Box)(({theme})=> ({
-    width: '90%',
-    height: 40,
-    margin: 'auto',
-    padding: '30px 0px',
-    borderTop: '2px solid #678c9265',
-    borderBottom: '2px solid #678c9265',
-    display: 'flex',
-    justifyContent: 'center',
-    [theme.breakpoints.down('md')]: {
-        height: 30,
-    },
-    [theme.breakpoints.down('sm')]: {
-        width: '100%',
-        height: 15,
-    }
+export const CompanyImageContainer = styled(Box)(({ theme }) => ({
+  width: '90%',
+  height: 40,
+  margin: 'auto',
+  padding: '30px 0px',
+  borderTop: '2px solid #678c9265',
+  borderBottom: '2px solid #678c9265',
+  display: 'flex',
+  justifyContent: 'center',
+  [theme.breakpoints.down('md')]: {
+    height: 30,
+  },
+  [theme.breakpoints.down('sm')]: {
+    width: '100%',
+    height: 15,
+  },
+}))
+
+// company details
+
+export const CompanyDetails = styled(Box)(({ theme }) => ({
+  display: 'grid',
+  gridTemplateColumns: 'repeat(2, 1fr)',
+  marginTop: '60px',
+  width: '100%',
+  [theme.breakpoints.down('md')]: {
+    gridTemplateColumns: 'repeat(1, 1fr)',
+  },
+}))
+export const CompanyImage = styled(Box)(({ theme }) => ({
+  padding: '55px',
+}))
+export const CompanyInfo = styled(Box)(({ theme }) => ({
+  padding: '0px 30px',
+}))
+
+export const CompanyInfoTitle = styled(Typography)(({ theme }) => ({
+  fontWeight: 'bold',
+  color: Colors.textColor,
+  padding: '20px 0px',
+}))
+
+export const CompanyInfoDescription = styled(Typography)(({ theme }) => ({
+  color: Colors.muted,
+  padding: '20px 0px',
 }))
