b11c732982c28f19efb8adf16fb1fb4c3a086a67
nazmulhassan-tmpl <nazmul.h@tunicalabsmedia.com>
2022-11-11 11:37:32 +0600

    completed instagram section

--- src/styles/homepage/instagram/index.js
+++ src/styles/homepage/instagram/index.js
@@ -7,17 +7,24 @@ export const InstagramContainer = styled(Box)(({ theme }) => ({
   background: Colors.secondary,
 }))
 
+export const InstagramContent = styled(Box)(({ theme }) => ({
+  padding: '50px 100px 25px 100px',
+  [theme.breakpoints.down('md')]: {
+    padding: '50px 30px 25px 30px',
+  }
+}))
+
 export const InstagramGridContainer = styled(Box)(({ theme }) => ({
   display: 'grid',
   gridTemplateColumns: 'repeat(3, 1fr)',
-  padding: '100px',
+  // padding: '100px',
   gap: 15,
   [theme.breakpoints.down('md')]: {
     gridTemplateColumns: 'repeat(2, 1fr)',
-    padding: '50px',
+    // padding: '50px',
   },
   [theme.breakpoints.down('sm')]: {
     gridTemplateColumns: 'repeat(1, 1fr)',
-    padding: '20px',
+    // padding: '20px',
   },
 }))
