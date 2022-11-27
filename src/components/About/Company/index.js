752f45066a2e2444c34f9fabfd8cbcea5f976ca6
nazmulhassan-tmpl <nazmul.h@tunicalabsmedia.com>
2022-11-28 01:38:15 +0600

    update company logo

--- src/components/About/Company/index.js
+++ src/components/About/Company/index.js
@@ -1,18 +1,58 @@
 import { Typography } from '@mui/material'
 import { Box } from '@mui/system'
-import { CompanyClient, CompanyContainer, CompanyHeading, CompanyTitle } from '../../../styles/about/company'
+import {
+  CompanyClient,
+  CompanyContainer,
+  CompanyHeading,
+  CompanyTitle,
+} from '../../../styles/about/company'
 import { Colors } from '../../../styles/theme'
+import Image from 'next/image'
+import LocationOnIcon from '@mui/icons-material/LocationOn'
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import CottageIcon from '@mui/icons-material/Cottage';
+import TouchAppIcon from '@mui/icons-material/TouchApp';
 
 export default function Company() {
   return (
-    <Box sx={{width: '100%', height: '100%'}}>
+    <Box sx={{ width: '100%', height: '100%' }}>
       <CompanyContainer>
         <CompanyTitle>
-          <Typography variant='body1' sx={{color: Colors.primary}}>Statistics</Typography>
+          <Typography variant='body1' sx={{ color: Colors.primary }}>
+            Statistics
+          </Typography>
           <CompanyHeading variant='h5'>Development in numbers</CompanyHeading>
         </CompanyTitle>
         <CompanyClient>
-            Client section here
+          {/* box 1  */}
+          <Box sx={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+            <Typography sx={{padding: '10px', background: '#678C92', borderRadius: '5px'}}>
+              <LocationOnIcon sx={{fontSize: '40px'}} />
+            </Typography>
+            <Box sx={{marginLeft: '10px',}}>
+              <Typography variant='h4' sx={{color: Colors.textColor}}> <ArrowForwardIosIcon />200</Typography>
+              <Typography variant='body2' sx={{color: Colors.primary, letterSpacing: '2px'}}>Number</Typography>
+            </Box>
+          </Box>
+          {/* box 2  */}
+          <Box sx={{ display: 'flex', alignItems: 'center', marginBottom: '10px' }}>
+            <Typography sx={{padding: '10px', background: '#678C92', borderRadius: '5px'}}>
+              <CottageIcon sx={{fontSize: '40px'}} />
+            </Typography>
+            <Box sx={{marginLeft: '10px',}}>
+              <Typography variant='h4' sx={{color: Colors.textColor}}> <ArrowForwardIosIcon />70</Typography>
+              <Typography variant='body2' sx={{color: Colors.primary, letterSpacing: '2px'}}>Number</Typography>
+            </Box>
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+            <Typography sx={{padding: '10px', background: '#678C92', borderRadius: '5px'}}>
+              <TouchAppIcon sx={{fontSize: '40px'}} />
+            </Typography>
+            <Box sx={{marginLeft: '10px',}}>
+              <Typography variant='h4' sx={{color: Colors.textColor}}> <ArrowForwardIosIcon />20</Typography>
+              <Typography variant='body2' sx={{color: Colors.primary, letterSpacing: '2px'}}>Number</Typography>
+            </Box>
+          </Box>
         </CompanyClient>
       </CompanyContainer>
     </Box>
--- src/styles/about/company/index.js
+++ src/styles/about/company/index.js
@@ -4,6 +4,12 @@ import { Colors } from '../../theme'
 
 export const CompanyContainer = styled(Box)(({ theme }) => ({
     padding: '100px',
+    [theme.breakpoints.down('md')]: {
+        padding: '60px', 
+    },
+    [theme.breakpoints.down('sm')]: {
+        padding: '20px', 
+    },
 }))
 
 export const CompanyTitle = styled(Box)(({ theme }) => ({
@@ -20,9 +26,22 @@ export const CompanyHeading = styled(Typography)(({ theme }) => ({
 }))
 
 export const CompanyClient = styled(Box)(({ theme }) => ({
-    width: '50%',
+    width: '70%',
     margin: '100px auto 0px auto',
-    background: Colors.primary,
+    background: 'linear-gradient(88.65deg, #C6DBFF 0%, #E1EBFA 100%)',
+    filter:'drop-shadow(0px 30px 60px rgba(59, 130, 246, 0.25))',
     padding: '50px',
-    color: 'white'
+    color: 'white',
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    [theme.breakpoints.down('md')]:{
+        gridTemplateColumns: 'repeat(2, 1fr)',
+        width: '80%',
+        padding: '30px',
+    },
+    [theme.breakpoints.down('sm')]:{
+        gridTemplateColumns: 'repeat(1, 1fr)',
+        width: '100%',
+            padding: '20px',
+    }
 }))
