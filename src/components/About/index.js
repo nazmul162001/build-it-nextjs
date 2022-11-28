523c2dcf679805cab2a3f797576b3948cfd78d84
nazmulhassan-tmpl <nazmul.h@tunicalabsmedia.com>
2022-11-28 10:34:47 +0600

    completed about hero section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,18 +1,42 @@
-import { AboutContainer, AboutHero, AboutHero2 } from '../../styles/about'
+import {
+  AboutContainer,
+  AboutContainerContent,
+  AboutHero,
+  AboutHero2,
+  AboutHeroTitle,
+} from '../../styles/about'
 import Image from 'next/image'
 import about from '/public/img/about/about2.svg'
 import about2 from '/public/img/about/about1.svg'
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
+import { Colors } from '../../styles/theme'
 
 export default function About() {
   return (
     <>
       <AboutContainer>
-        <AboutHero>
-          {/* <Image src={about}  layout='responsive' alt=''></Image> */}
-          Image
-        </AboutHero>
-        <AboutHero2>Content</AboutHero2>
+        <AboutContainerContent>
+          <AboutHero>
+            <Image src={about} className='custom-image' alt=''></Image>
+          </AboutHero>
+          <AboutHero2>
+            <Typography sx={{ color: Colors.primary }}>About Us</Typography>
+            <AboutHeroTitle variant='h4'>
+              Owner and investor with a reputation
+            </AboutHeroTitle>
+            <Typography
+              variant='body2'
+              sx={{ color: Colors.muted, padding: '5px 0px' }}
+            >
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc elit
+              sem, vestibulum sed leo eu, malesuada pharetra mauris. Integer
+              rhoncus, eros vel euismod tempor, ipsum magna tristique nisi, quis
+              ullamcorper enim magna eu orci. Sed semper ex quis semper aliquet.
+              Cras hendrerit molestie sapien sed fermentum. Mauris dui tortor,
+              viverra vel ultrices in, congue sed ex.
+            </Typography>
+          </AboutHero2>
+        </AboutContainerContent>
       </AboutContainer>
     </>
   )
--- src/styles/about/index.js
+++ src/styles/about/index.js
@@ -4,15 +4,23 @@ import { Colors } from '../theme'
 
 export const AboutContainer = styled(Box)(({ theme }) => ({
   width: '100%',
-  height: '30vh',
+  height: '100%',
   background: Colors.secondary,
+  paddingTop: '60px',
+}))
+export const AboutContainerContent = styled(Box)(({ theme }) => ({
   display: 'grid',
   gridTemplateColumns: 'repeat(2, 1fr)',
   justifyContent: 'center',
   alignItems: 'center',
-  padding: '120px 60px 60px 60px',
+  padding: '60px',
   [theme.breakpoints.down('md')]: {
     gridTemplateColumns: 'repeat(1, 1fr)',
+    padding: '40px',
+  },
+  [theme.breakpoints.down('md')]: {
+    gridTemplateColumns: 'repeat(1, 1fr)',
+    padding: '20px',
   },
 }))
 
@@ -20,9 +28,26 @@ export const AboutHero = styled(Box)(({ theme }) => ({
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
+
 }))
 export const AboutHero2 = styled(Box)(({ theme }) => ({
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
+  paddingRight: '50px',
+  [theme.breakpoints.down('md')]: {
+    paddingRight: '30px',
+  },
+  [theme.breakpoints.down('sm')]: {
+    paddingRight: '5px',
+  },
+}))
+
+export const AboutHeroTitle = styled(Typography)(({ theme }) => ({
+  color: Colors.textColor,
+  fontWeight: 'bold',
+  fontSize: '36px',
+  [theme.breakpoints.down('md')]: {
+    fontSize: '28px',
+  },
+  [theme.breakpoints.down('sm')]: {
+    fontSize: '18px',
+  },
 }))
