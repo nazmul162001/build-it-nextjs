6b6859507dead049b5ec970be3feeb365098c3dd
nazmulhassan-tmpl <nazmul.h@tunicalabsmedia.com>
2022-12-03 15:56:21 +0600

    update credential

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,8 @@ import '../styles/globals.css'
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../styles/theme'
 
+//my all component pages load here
+
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme} >
